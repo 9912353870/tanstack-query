514167f649728df1d1caec247d5b0398a16ba2eb
Bhanu Prakash M <sravan8147@gmail.com>
2024-09-18 19:40:03 +0530

    Added tanstack query chnages

--- src/components/PostRQ.jsx
+++ src/components/PostRQ.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
 
 const PostRQ = () => {
-  return (
-    <div className="container">PostRQ</div>
-  )
-}
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => {
+      return axios.get("http://localhost:4000/posts");
+    },
+  });
 
-export default PostRQ
+  if (isError) return <div>{error.message}</div>;
+  if (isLoading) return <div>Loading....!</div>;
+
+  return (
+    <div className="post-list">
+      {data &&
+        data?.data?.map((item) => {
+          return (
+            <div className="post-item" key={item.id}>
+              <h3 className="post-title">{item.title}</h3>
+              <p className="post-body">{item.body}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default PostRQ;
